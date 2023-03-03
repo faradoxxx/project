5b1fd6ec1e7aa0c3d4aad48a15e92651622439cf
Farah Eunice Perias <117723441+faradoxxx@users.noreply.github.com>
2023-03-03 22:04:33 +0800

    first commit

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import '../../asset/css/custom.css'
+
+export class TopBanner extends Component {
+  render() {
+    return (
+        <Fragment>
+             <Container className='topFixedBanner'>
+                <Row>
+
+                </Row>
+             </Container>
+        </Fragment>
+    )
+  }
+}
+
+export default TopBanner
