4964436535595f2267c3e69db0dba13ca6bffb15
Farah Eunice Perias <117723441+faradoxxx@users.noreply.github.com>
2023-03-03 23:01:10 +0800

    Add TopBanner

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,15 +1,24 @@
 import React, { Component, Fragment } from 'react'
-import { Container, Row } from 'react-bootstrap'
-import '../../asset/css/custom.css'
+import { Col, Container, Row, Button} from 'react-bootstrap'
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
 
 export class TopBanner extends Component {
   render() {
     return (
         <Fragment>
-             <Container className='topFixedBanner'>
-                <Row>
-
-                </Row>
+             <Container fluid={true} className="topFixedBanner p-0">
+                <div className="topBannerOverlay">
+                <Container className="topContent">
+                    <Row>
+                        <Col className="text-center">
+                        <h1 className='topTitle'>EASY LEARNING</h1>
+                        <h4 className='topSubTitle'>Learn Professionally</h4>
+                        <Button variant="primary">Learn More</Button>
+                        </Col>
+                    </Row>
+                </Container>
+                </div>
              </Container>
         </Fragment>
     )
